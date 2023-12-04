40b5b09b5309638c21ec9e8aeb4afddea0bd8f8e
PedroLzr <cereus992@gmail.com>
2023-12-04 20:09:49 +0100

    add hangman game

--- src/index.py
+++ src/index.py
@@ -2,7 +2,7 @@ from flask import Flask, render_template
 from controllers.index import getToday
 from api.api import api_bp
 
-app = Flask(__name__)
+app = Flask(__name__, static_folder="static")
 app.register_blueprint(api_bp, url_prefix='/api')
 
 @app.route('/', methods=['GET'])
@@ -17,6 +17,10 @@ def credits():
 @app.route('/ourApis', methods=['GET'])
 def ourApis():
     return render_template('pages/our_apis.html')
+
+@app.route('/hangman', methods=['GET'])
+def hangman():
+    return render_template('pages/hangman.html')
 
 if __name__ == '__main__':
     app.run()
---
+++ src/static/scripts/hangman.js
@@ -0,0 +1,96 @@
+document.addEventListener("DOMContentLoaded", function () {
+
+    // Lista de palabras para el juego
+    const words = ["javascript", "html", "css", "nodejs", "react", "angular", "python", "java", "typescript", "php"];
+
+    // Escoge una palabra al azar de la lista
+    let selectedWord = words[Math.floor(Math.random() * words.length)];
+
+    // Inicialización de variables
+    let guessedLetters = [];
+    let hangmanImage = 0;
+    const wordDisplay = document.getElementById("word-display");
+    const lettersContainer = document.getElementById("letters-container");
+    const hangmanImageElement = document.getElementById("hangman-image");
+
+    // Función para mostrar la palabra oculta con guiones bajos y letras adivinadas
+    function displayWord() {
+        let displayText = "";
+        for (let letter of selectedWord) {
+            letter = letter.toUpperCase();
+            if (guessedLetters.includes(letter)) {
+                displayText += letter + " ";
+            } else {
+                displayText += "_ ";
+            }
+        }
+        wordDisplay.textContent = displayText;
+    }
+
+    // Función para actualizar la imagen del ahorcado
+    function updateHangmanImage() {
+        let imageUrl = "/static/images/hangman/" + hangmanImage + ".png";
+        hangmanImageElement.src = imageUrl;
+    }
+
+    // Función para verificar si el jugador ha ganado
+    function checkWin() {
+        if (!wordDisplay.textContent.includes("_")) {
+            alert("¡Has ganado!");
+            resetGame();
+        }
+    }
+
+    // Función para verificar si el jugador ha perdido
+    function checkLose() {
+        if (hangmanImage === 6) {
+            alert("¡Has perdido! La palabra era: " + selectedWord);
+            resetGame();
+        }
+    }
+
+    // Función para reiniciar el juego
+    function resetGame() {
+        guessedLetters = [];
+        hangmanImage = 0;
+        selectedWord = words[Math.floor(Math.random() * words.length)];
+        displayWord();
+        updateHangmanImage();
+    }
+
+    // Event listener para manejar los clics en las letras
+    lettersContainer.addEventListener("click", function (event) {
+        if (event.target.classList.contains("letter-button")) {
+            let letter = event.target.textContent;
+            if (!guessedLetters.includes(letter)) {
+                guessedLetters.push(letter);
+                event.target.disabled = true;
+                displayWord();
+
+                letter = letter.toUpperCase();
+                selectedWord = selectedWord.toUpperCase();
+                
+                if (!selectedWord.includes(letter)) {
+                    hangmanImage++;
+                    updateHangmanImage();
+                    checkLose();
+                } else {
+                    checkWin();
+                }
+            }
+        }
+    });
+
+    // Inicialización del juego
+    displayWord();
+
+    // Creación de botones de letras
+    for (let i = 65; i <= 90; i++) {
+        const letter = String.fromCharCode(i);
+        const button = document.createElement("button");
+        button.textContent = letter;
+        button.classList.add("letter-button");
+        lettersContainer.appendChild(button);
+    }
+
+});
