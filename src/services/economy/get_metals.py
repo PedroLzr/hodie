de8eae475dee51a831b1b6e86a4d97f66819a5da
PedroLzr <cereus992@gmail.com>
2024-01-30 18:02:18 +0100

    style price metals

--- src/services/economy/get_metals.py
+++ src/services/economy/get_metals.py
@@ -5,5 +5,16 @@ def get_metals():
     client = MetalsDev()
 
     metals = client.get_metal_prices_latest()
+    format_metals = format_nums(metals)
+    return format_metals
 
+def format_nums(metals):
+    if isinstance(metals, dict):
+        for clave, valor in metals.items():
+            metals[clave] = format_nums(valor)
+    elif isinstance(metals, list):
+        for i, elemento in enumerate(metals):
+            metals[i] = format_nums(elemento)
+    elif isinstance(metals, (int, float)):
+        metals = "{:,.4f}".format(metals).replace(",", " ").replace(".", ",").replace(" ", ".")
     return metals
