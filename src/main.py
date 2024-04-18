0de537c7454f4152cd8aef804f00ea054590ad60
PedroLzr <cereus992@gmail.com>
2024-04-18 10:13:55 +0200

    jsons and delete code commented

--- src/main.py
+++ src/main.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, redirect
+from flask import Flask
 from routes.index import index_bp
 from routes.economy import economy_bp
 from routes.credits import credits_bp
@@ -7,16 +7,6 @@ from routes.games import games_bp
 from routes.api_culture_v1 import api_culture_v1_bp
 
 app = Flask(__name__, static_folder="static")
-
-# Esta función redirige las request http a https
-# Esto es temporal, lo correcto es:
-# Instalar nginx en la vps y configurarlo para que haga este comportamiento
-# @app.before_request
-# def before_request():
-#     if not request.is_secure:
-#         url = request.url.replace('http://', 'https://', 1)
-#         code = 301
-#         return redirect(url, code=code)
 
 # Registro de Blueprints
 app.register_blueprint(index_bp)
